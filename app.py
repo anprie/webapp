687365aafa621e2fbd7bde59ccd5c75a6bce8573
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-11-11 15:18:18 +0100

    here we go.

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, redirect, url_for, request, render_template
+from markupsafe import escape
+import sys
+sys.path.insert(0, '/home/antonia/repositories/anagram2/')
+import os
+workdir = os.path.abspath( os.path.dirname( __file__ ) )
+
+from anagram import Anagram
+
+app = Flask(__name__)#, template_folder='templates' )
+
+
+@app.route('/')
+def index():
+    #return render_template('index.html')
+    return 'index'
+
+#@app.route('/anagram/<results>')
+#def anagram(results):
+@app.route('/anagram/<word>_<language>_<results>/')
+def anagram(word, language, results):
+    #print("word: ", word)
+    #print("language: ", language)
+    #print("results: ", results)
+    return render_template('anagram.html', results=results, word=word, language=language)
+
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    if request.method == 'POST':
+        word = escape(request.form['word'])
+        language = escape(request.form['language'])
+        results = Anagram.process(word, workdir +'/../anagram2/'+ language)
+        print("results: ", results)
+        return redirect(url_for('anagram', word=word, language=language, results=results))
+    else:
+        return render_template('form.html')
+
+with app.test_request_context():
+    print(url_for('form'))
+    print(url_for('anagram', word='foo', language='bar', results='testtest'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
