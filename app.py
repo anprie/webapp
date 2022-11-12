22ebefd7faff75003ba799b54424ba2942e1ba98
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-11-12 21:04:47 +0100

    return sorted anagrams, without commas and quotations marks, separated by newlines.

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def index():
 
 @app.route('/anagram/<word>_<language>_<results>/')
 def anagram(word, language, results):
+    results = "\n".join(sorted(results.split("\', \'"))).strip('{\'}')
     return render_template('anagram.html', results=results, word=word, language=language)
 
 @app.route('/form', methods=['POST', 'GET'])
