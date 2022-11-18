8cbb0cd29e5468989f746fbfa02c9fe4ef2a37aa
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-11-18 11:50:03 +0100

    add javascript file for drag and drop. Not working yet.

---
+++ static/drag.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+function allowDrop(e) {
+    e.preventDefault();
+}
+
+function drag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+
+
+function drop(e) {
+    var fetchData = e.dataTransfer.getData("text");
+    e.target.appendChild(document.getElementById(fetchData));
+
+function dragStart(e) {
+    e.dataTransfer.effectAllowed='move';
+    e.dataTransfer.setData("text", e.target.getAttribute('id'));
+    e.dataTransfer.setDragImage(e.target,0,0);
+    return true;
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    return true;
+}
+
+function dragOver(e) {
+    return false;
+}
+
+function dragDrop(e) {
+    var src = e.dataTransfer.getData("text");
+    e.target.appendChild(document.getElementById(src));
+    e.stopPropagation();
+    return false;
+}
+
+console.log("hello world!")
